1b611010e32f538a41bfc80ffb5f7f8775316d84
Kosuke Imai <koimai@c1r18s5.42tokyo.jp>
2023-10-21 18:12:39 +0900

    modified get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: koimai <koimai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 20:28:11 by koimai            #+#    #+#             */
-/*   Updated: 2023/10/21 17:56:33 by koimai           ###   ########.fr       */
+/*   Updated: 2023/10/21 18:12:03 by koimai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,29 +85,17 @@ char	*get_next_line(int fd)
 // {
 // 	char	*line;
 // 	int		i;
-// 	int		fd1;
-// 	int		fd2;
-// 	int		fd3;
+// 	int		fd;
 
-// 	fd1 = open("tests/test.txt", O_RDONLY);
-// 	fd2 = open("tests/test2.txt", O_RDONLY);
-// 	fd3 = open("tests/test3.txt", O_RDONLY);
+// 	fd = open("tests/test.txt", O_RDONLY);
 // 	i = 1;
 // 	while (i < 7)
 // 	{
-// 		line = get_next_line(fd1);
-// 		printf("line [%02d]: %s", i, line);
-// 		free(line);
-// 		line = get_next_line(fd2);
-// 		printf("line [%02d]: %s", i, line);
-// 		free(line);
-// 		line = get_next_line(fd3);
-// 		printf("line [%02d]: %s", i, line);
+// 		line = get_next_line(fd);
+// 		printf("%d:%s", i, line);
 // 		free(line);
 // 		i++;
 // 	}
-// 	close(fd1);
-// 	close(fd2);
-// 	close(fd3);
+// 	close(fd);
 // 	return (0);
 // }
